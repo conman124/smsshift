aa1ebc862585ff3c55bd5659ef265d1d26dc6239
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-24 21:46:25 -0700

    Added gulpfile.

---
+++ chrome/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+gulp.task('html', () =>
+  gulp.src('html/*').pipe(gulp.dest('build/html'))
+);
+
+gulp.task('css', () =>
+  gulp.src('css/*').pipe(gulp.dest('build/css'))
+);
+
+gulp.task('js', () =>
+  gulp.src('js/*').pipe(gulp.dest('build/js'))
+);
+
+gulp.task('vendorjs', () => {
+  gulp.src('node_modules/lodash/dist/lodash.min.js').pipe(gulp.dest('build/js'));
+});
+
+gulp.task('default', ['html', 'css', 'js', 'vendorjs']);
+
+gulp.task('clean', () =>
+  gulp.src('build', {read: false}).pipe(clean())
+)
