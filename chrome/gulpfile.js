697a951322ffa725aab443a45401a391fa9bb710
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-25 16:05:44 -0700

    Added manifest.json.

--- chrome/gulpfile.js
+++ chrome/gulpfile.js
@@ -9,6 +9,10 @@ gulp.task('css', () =>
   gulp.src('css/*').pipe(gulp.dest('build/css'))
 );
 
+gulp.task('manifest', () =>
+  gulp.src(["manifest.json","background.html"]).pipe(gulp.dest('build'))
+);
+
 gulp.task('js', () =>
   gulp.src('js/*').pipe(gulp.dest('build/js'))
 );
@@ -17,7 +21,7 @@ gulp.task('vendorjs', () => {
   gulp.src('node_modules/underscore/underscore-min.js').pipe(gulp.dest('build/js'));
 });
 
-gulp.task('default', ['html', 'css', 'js', 'vendorjs']);
+gulp.task('default', ['html', 'css', 'manifest', 'js', 'vendorjs']);
 
 gulp.task('clean', () =>
   gulp.src('build', {read: false}).pipe(clean())
