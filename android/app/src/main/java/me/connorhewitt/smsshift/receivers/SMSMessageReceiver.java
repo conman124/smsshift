09ba576a14b2ed9e8174b97d9299cdb0a0dc9ac3
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-27 23:29:29 -0700

    Android app sends SMSes upstream!

--- android/app/src/main/java/me/connorhewitt/smsshift/receivers/SMSMessageReceiver.java
+++ android/app/src/main/java/me/connorhewitt/smsshift/receivers/SMSMessageReceiver.java
@@ -5,21 +5,54 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
-import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+
+import me.connorhewitt.smsshift.R;
 
 /**
  * Created by connor on 12/27/17.
  */
 
 public class SMSMessageReceiver extends BroadcastReceiver {
-
 	private final static String TAG = "SMSMessageReceiver";
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		final SmsMessage[] messages = Telephony.Sms.Intents.getMessagesFromIntent(intent);
 		for (SmsMessage message : messages) {
-			Log.d(TAG, String.format("Message from %s, body: \"%s\"", message.getDisplayOriginatingAddress(), message.getDisplayMessageBody()));
+			onSMSReceived(context, message);
+		}
+	}
+
+	private void onSMSReceived(Context context, SmsMessage smsMessage) {
+		SmsReceivedEvent ev = new SmsReceivedEvent(smsMessage);
+		String sender = context.getString(R.string.gcm_defaultSenderId);
+
+		FirebaseMessaging.getInstance().send(
+			new RemoteMessage.Builder(sender + "@gcm.googleapis.com")
+				.setMessageId(ev.senderNumber + "_" + ev.timestamp)
+				.addData("msg_type", "received")
+				.addData("msg", new Gson().toJson(ev))
+			.build()
+		);
+	}
+
+	private static class SmsReceivedEvent {
+		public final String senderNumber;
+		public final String senderName;
+		public final String message;
+		public final long timestamp;
+		public final int color;
+
+		private SmsReceivedEvent(SmsMessage smsMessage) {
+			senderNumber = smsMessage.getDisplayOriginatingAddress();
+			senderName = null;
+			message = smsMessage.getDisplayMessageBody();
+			timestamp = smsMessage.getTimestampMillis();
+			color = 0;
 		}
 	}
 }
