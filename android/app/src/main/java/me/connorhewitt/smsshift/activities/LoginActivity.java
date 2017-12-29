b5d3b6967f1f50f3b10da8341ddb49b2e4c37efc
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-28 23:35:57 -0700

    Added button which sends a message to my email.

--- android/app/src/main/java/me/connorhewitt/smsshift/activities/LoginActivity.java
+++ android/app/src/main/java/me/connorhewitt/smsshift/activities/LoginActivity.java
@@ -3,7 +3,9 @@ package me.connorhewitt.smsshift.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.iid.FirebaseInstanceId;
@@ -11,7 +13,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import me.connorhewitt.smsshift.Manifest;
 import me.connorhewitt.smsshift.R;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
 
 	private final static int SMS_PERMISSION_REQUEST = 0;
 
@@ -20,9 +22,11 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 
+		this.findViewById(R.id.button).setOnClickListener(this);
+
 		Log.d("LoginActivity", FirebaseInstanceId.getInstance().getToken());
 
-		this.requestPermissions(new String[] {android.Manifest.permission.RECEIVE_SMS, android.Manifest.permission.READ_SMS}, SMS_PERMISSION_REQUEST);
+		this.requestPermissions(new String[] {android.Manifest.permission.RECEIVE_SMS, android.Manifest.permission.READ_SMS, android.Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST);
 	}
 
 	@Override
@@ -30,5 +34,11 @@ public class LoginActivity extends Activity {
 		if(requestCode == SMS_PERMISSION_REQUEST) {
 			Toast.makeText(this, "SMS permission granted.", Toast.LENGTH_LONG).show();
 		}
+	}
+
+	@Override
+	public void onClick(View view) {
+		final SmsManager smsManager = SmsManager.getDefault();
+		smsManager.sendTextMessage(smsManager.getCarrierConfigValues().getString("emailGatewayNumber"), null, "connor.hewitt@gmail.com Testing!", null, null);
 	}
 }
