674b048be4e94c9348b5989d3c1610b54dd405dd
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-29 18:13:03 -0700

    Added function to send a text message from Chrome app.  Closes #3.

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,7 +1,9 @@
-import {registerGCM} from './gcm.js';
+import {registerGCM, sendTextMessage} from './gcm.js';
 
 chrome.gcm.onMessage.addListener(function(message) {
   console.log(message.data);
 });
 
 registerGCM();
+
+window.sendTextMessage = sendTextMessage;
--- chrome/js/gcm.js
+++ chrome/js/gcm.js
@@ -17,4 +17,31 @@ function registerGCM() {
   });
 }
 
-export {registerGCM};
+function sendGcmPromise(msg) {
+  return new Promise((resolve, reject) => {
+    chrome.gcm.send(msg, (id) => {
+      if(chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      return resolve(id);
+    })
+  })
+}
+
+function sendTextMessage(to, message) {
+  return senderPromise.then((sender) =>
+    sendGcmPromise({
+      destinationId: sender + "@gcm.googleapis.com",
+      messageId: to + "_" + Date.now(),
+      timeToLive: 0,
+      data: {
+        type: "send",
+        event: JSON.stringify({
+          to, message
+        })
+      }
+    })
+  )
+}
+
+export {registerGCM, sendTextMessage};
