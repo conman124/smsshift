8c7b8253c2cb4ce9eccdfda2b4c312a1dfcdc583
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-30 15:46:34 -0700

    Got webpack working more.

--- chrome/webpack.config.dev.js
+++ chrome/webpack.config.dev.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   entry: {
     background: './js/background.js'
@@ -5,5 +8,34 @@ module.exports = {
   output: {
     path: __dirname + '/build/js',
     filename: "[name].js"
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      chunks: ['background'],
+      filename: '../html/background.html'
+    }),
+    new CopyWebpackPlugin([{
+      from: 'manifest.json',
+      to: '../'
+    }])
+  ]
 }
