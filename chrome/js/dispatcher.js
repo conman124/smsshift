39d05e7cffe046da25536d8d3cb60720c0383580
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-31 16:46:51 -0700

    Took ACTIONS out of dispatcher, it was probably a bad idea.

--- chrome/js/dispatcher.js
+++ chrome/js/dispatcher.js
@@ -1,7 +1,3 @@
 import {Dispatcher} from 'flux';
-import actionTypes from './actionTypes.js';
-
-const dispatcher = new Dispatcher();
-dispatcher.ACTIONS = actionTypes;
 
 export default new Dispatcher();
