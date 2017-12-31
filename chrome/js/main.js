f70f9b08780239cb72329b98b62d6ea2f89b7e3f
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-30 20:57:12 -0700

    Added base App component.

--- chrome/js/main.js
+++ chrome/js/main.js
@@ -1,1 +1,12 @@
-console.log('heyo');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+let mainElement = document.createElement('main');
+mainElement.id = "main";
+document.body.appendChild(mainElement)
+
+ReactDOM.render(
+  React.createElement(App, null),
+  document.getElementById("main")
+);
