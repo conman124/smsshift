ee4c71a9942a9fe8a0ed974c8fddde93bc53b036
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-30 20:15:06 -0700

    Show main.html on app launch.

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -4,6 +4,15 @@ chrome.gcm.onMessage.addListener(function(message) {
   console.log(message.data);
 });
 
+chrome.app.runtime.onLaunched.addListener(() => {
+  chrome.app.window.create('html/main.html', {
+    outerBounds: {
+      width: 640,
+      height: 480
+    }
+  })
+})
+
 registerGCM();
 
 window.sendTextMessage = sendTextMessage;
