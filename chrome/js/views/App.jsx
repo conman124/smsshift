7584552e6081c8b80c31c0999aa4ea2ed96236ce
Connor Hewitt <connor.hewitt@gmail.com>
2017-12-31 23:28:17 -0700

    Changed App to support a conversation list and a conversation.

--- chrome/js/views/App.jsx
+++ chrome/js/views/App.jsx
@@ -1,28 +1,50 @@
 import React from 'react';
 
 function Person(props) {
+  let child = props.senderNumber;
+
+  if(props.currentConversation === props.senderNumber) {
+    child = <em>{child}</em>;
+  }
+
   return (
-    <li>
-      <em>{props.senderNumber}</em>
-      <ul>
-        {
-          props.messages.map((msg) =>
-            <li key={msg.senderNumber+"_"+msg.timestamp}>{msg.message}</li>
-          )
-        }
-      </ul>
-    </li>
+    <li>{child}</li>
+  )
+}
+
+function ConversationList(props) {
+  return (
+    <ul>
+      {
+        props.sms.keySeq().reverse().map((sender) =>
+          <Person key={sender} senderNumber={sender} currentConversation={props.currentConversation} />
+        )
+      }
+    </ul>
+  )
+}
+
+function Conversation(props) {
+  if(!props.currentConversation) {
+    return null;
+  }
+
+  return (
+    <ul>
+      {
+        props.sms.get(props.currentConversation).map((message) =>
+          <li key={message.timestamp}>{message.message}</li>
+        )
+      }
+    </ul>
   )
 }
 
 export default function App(props) {
   return (
-    <ul>
-      {
-        props.sms.keySeq().reverse().map((sender) =>
-          <Person key={sender} senderNumber={sender} messages={props.sms.get(sender)} />
-        )
-      }
-    </ul>
-  )
+    <div>
+      <ConversationList {...props} />
+      <Conversation {...props} />
+    </div>
+  );
 }
