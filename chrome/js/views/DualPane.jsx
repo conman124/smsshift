8872dcffc58226d98c507a9ce0b31cb801338895
Connor Hewitt <connor.hewitt@gmail.com>
2018-01-01 14:06:47 -0700

    Add DualPane component.

---
+++ chrome/js/views/DualPane.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+
+const styles = {
+  container: {
+    display: "flex",
+    height: "100%"
+  },
+  left: {
+    flex: "0 0 240px"
+  },
+  right: {
+    flex: "1"
+  }
+};
+
+class DualPane extends React.Component {
+  constructor(props) {
+    super(props);
+
+    if(!this.props.left || !React.isValidElement(this.props.left) || !this.props.right || !React.isValidElement(this.props.right)) {
+      throw new Error("DualPane needs a left and a right child!");
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.props.classes.container}>
+        <div className={this.props.classes.left}>{this.props.left}</div>
+        <div className={this.props.classes.right}>{this.props.right}</div>
+      </div>
+    )
+  }
+}
+
+export default injectSheet(styles)(DualPane);
